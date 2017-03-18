ba42ff22cb005539dee764d6cfcb49d3ba04105d
Nancy <nancatech@gmail.com>
2017-03-18 19:01:13 -0400

    first round of BEM styling

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
-
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function(){
   console.log('nanca rocks');
@@ -16,7 +16,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))  //array of postcss filters
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
